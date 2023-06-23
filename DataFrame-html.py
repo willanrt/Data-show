9bc9521aa01a650f2a7fa83832c88b86981fae6a
daiyifan <f1241726242@outlook.com>
2023-06-23 15:27:55 +0800

    Delete DataFrame-html.py

--- DataFrame-html.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-# 读取之前爬取的表格数据
-table = pd.read_csv("Nutrition__Physical_Activity__and_Obesity_-_Behavioral_Risk_Factor_Surveillance_System.csv")
-# 将表格数据转换成HTML格式
-html = table.to_html()
-# 打印HTML格式的表格数据
-print(html)
